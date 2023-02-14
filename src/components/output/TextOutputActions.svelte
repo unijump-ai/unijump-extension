9c86f82d8639d91045776b8e7e5a3bb6091a6351
Doga Genc <dogagenc@gmail.com>
2023-02-14 18:30:18 +0300

    Write copied label on output copied to clipboard

--- src/components/output/TextOutputActions.svelte
+++ src/components/output/TextOutputActions.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
+  import { OutputAction } from '$lib/prompt/output.constants';
+  import { createEventDispatcher } from 'svelte';
+  import { sleep } from '$lib/utils';
   import IconCopy from '$assets/icons/copy.svg?component';
   import IconRefresh from '$assets/icons/refresh.svg?component';
   import IconShorten from '$assets/icons/shorten.svg?component';
   import IconExpand from '$assets/icons/expand.svg?component';
   import Button from '$components/elements/Button.svelte';
-  import { OutputAction } from '$lib/prompt/output.constants';
-  import { createEventDispatcher } from 'svelte';
 
   export let output: string;
 
   const dispatch = createEventDispatcher();
 
-  function copyOutputToClipboard() {
+  let copied = false;
+
+  async function copyOutputToClipboard() {
+    if (copied) return;
+
     navigator.clipboard.writeText(output);
+    copied = true;
+    await sleep(2000);
+    copied = false;
   }
 
   function dispatchAction(action: OutputAction) {
@@ -22,7 +30,8 @@
 
 <div class="grid grid-cols-2 gap-2">
   <Button on:click={copyOutputToClipboard}>
-    <IconCopy width={16} /> Copy
+    <IconCopy width={16} />
+    {copied ? 'Copied!' : 'Copy'}
   </Button>
   <Button on:click={() => dispatchAction(OutputAction.REWRITE)}>
     <IconRefresh width={16} /> Rewrite
